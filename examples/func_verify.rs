90e1bcdffa777fad2537b1715f8206735936848a
liujiajin <jiajin.liu@glightmed.com>
2024-08-19 19:57:42 +0800

    存档2

--- examples/func_verify.rs
+++ examples/func_verify.rs
@@ -2,11 +2,24 @@
  * @Author: liujiajin
  * @Date: 2024-08-17 10:58:54
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2024-08-17 14:10:31
+ * @LastEditTime: 2024-08-19 17:39:49
  * @Description: 
  */
 extern crate rfcaf;
 
 fn main() {
+    let mut test = rfcaf::Console::new();
+    println!("{:?}", test);
+    // if let Err(err_info) = test.file_import("example.toml") {
+    //     println!("{:?}", err_info);
+    // }
+    test.refresh();
+    
+    loop {
+
+        test.read("输入一条命令");
+
+    }
+
     println!("hello world!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,39 @@
  * @Author: likkoliu
  * @Date: 2024-08-17 10:48:48
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2024-08-17 18:48:55
+ * @LastEditTime: 2024-08-19 19:54:35
  * @Description:
  */
-use std::io::{self, Write};
+use serde_derive::Deserialize;
+use std::{
+    io::{self, Write},
+    vec,
+};
+use thiserror::Error;
+use toml;
 
+#[derive(Error, Debug)]
+pub enum DataError {
+    #[error("data loss")]
+    Loss(#[from] io::Error),
+    #[error("the data for key `{0}` is not available")]
+    Redaction(String),
+    #[error("invalid header (expected {expected:?}, found {found:?})")]
+    InvalidHeader { expected: String, found: String },
+    #[error("unknown data error")]
+    Unknown,
+}
+
+/// Supported command data types
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum GenericCmd {
+    Number(u8),
+    Character(String),
+}
 
 /// Console Status
+#[derive(Debug)]
 pub enum ConsoleStatus {
     InsAcqFromFile,     // Instruction acquisition status
     InsAcqFromTerminal, // Instruction acquisition status
@@ -16,47 +42,67 @@ pub enum ConsoleStatus {
     InsExecFromFile,     // Instruction execution status
     InsExecFromTerminal, // Instruction execution status
 
-    Idle, // Idle state
+    Invaild, // Invaild state
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ValidCheck {
+    read_valid: bool,   // Command read valid
+    import_valid: bool, // Import file is valid
+    file_valid: bool,   // The file address has been obtained
 }
 
 /// Command line echo prompt
+#[derive(Debug)]
 pub struct ConsolePrompt {
     mian_prompt: String,
     sub_prompt: String,
 }
 
 /// Automation command execution file config
-pub struct ExcuteFile<'a, T> {
+#[derive(Deserialize, Debug)]
+pub struct ExcuteFile {
     file_address: Option<String>, // Automatic execution command file address
 
-    exc_ins_assets: Option<Vec<ExcuteAssets>>, // Automatically execute instructions and command assets
-    cycle_times: Option<u8>,                   // Automatic execution cycle times
+    exc_ins_assets: Vec<ExcuteAssets>, // Automatically execute instructions and command assets
+    cycle_times: Option<u8>,           // Automatic execution cycle times
 
-    next_exc_ins: Option<&'a T>, // Next automatic execution instruction
-    next_exc_cmd: Option<&'a T>, // Next auto-execute command
+    next_exc_ins: Option<String>, // Next automatic execution instruction
+    next_exc_cmd: Option<String>, // Next auto-execute command
 }
 
+#[derive(Deserialize, Debug)]
 struct ExcuteAssets {
-    exc_ins: Option<String>,      // Automatic execution instruction
-    sub_cmd: Option<Vec<String>>, // Auto-execute command assets
+    exc_ins: Option<GenericCmd>, // Automatic execution instruction
+    sub_cmd_assets: Vec<SubCmd>, // Auto-execute command assets
 }
 
-struct Console<'a, T> {
+#[derive(Deserialize, Debug)]
+struct SubCmd {
+    sub_cmd: GenericCmd,
+}
+
+#[derive(Debug)]
+pub struct Console {
     current_status: ConsoleStatus,
-    read_valid: bool,
+    check: ValidCheck,
     interact_prompt: ConsolePrompt,
 
-    current_ins: Option<&'a T>, // Currently executing instruction
-    current_cmd: Option<&'a T>, // Currently executing command
+    current_ins: Option<String>, // Currently executing instruction
+    current_cmd: Option<String>, // Currently executing command
 
-    auto_exc: ExcuteFile<'a, T>,
+    auto_exc: ExcuteFile,
 }
 
-impl<'a, T> Console<'a, T> {
+impl Console {
     pub fn new() -> Self {
         Console {
-            current_status: ConsoleStatus::Idle,
-            read_valid: false,
+            current_status: ConsoleStatus::Invaild,
+            check: ValidCheck {
+                read_valid: false,
+                import_valid: false,
+                file_valid: false,
+            },
             interact_prompt: ConsolePrompt {
                 mian_prompt: String::from("> "),
                 sub_prompt: String::from(""),
@@ -67,7 +113,7 @@ impl<'a, T> Console<'a, T> {
 
             auto_exc: ExcuteFile {
                 file_address: None,
-                exc_ins_assets: None,
+                exc_ins_assets: Vec::new(),
                 cycle_times: None,
                 next_exc_ins: None,
                 next_exc_cmd: None,
@@ -80,57 +126,163 @@ impl<'a, T> Console<'a, T> {
     }
 
     pub fn taildowm(&mut self) {
-        self.current_status = ConsoleStatus::Idle;
+        self.current_status = ConsoleStatus::Invaild;
     }
-
-    fn ask_excute_file(&mut self) {}
 
     fn input_paser(input: String) -> String {
         let x: &[_] = &['\r', '\n'];
         return String::from(input.trim_end_matches(x));
     }
 
-    pub fn terminal_read(&mut self, prompt: &str)<T> {
-        self.log(&format!("{}{}", self.interact_prompt.mian_prompt, prompt));
+    pub fn terminal_read(&mut self, prompt: &str) -> Result<GenericCmd, DataError> {
+        self.log(&format!(
+            "{}{}{}",
+            self.interact_prompt.mian_prompt, self.interact_prompt.sub_prompt, prompt
+        ));
 
         let _ = io::stdout().flush();
         let mut input = String::new();
-        if let Ok(_) = io::stdin().read_line(&mut input) {
-            input = Console::<&str>::input_paser(input);
-        } else {
-            input = String::from("");
+        io::stdin()
+            .read_line(&mut input)
+            .map_err(|_| DataError::InvalidHeader {
+                expected: ("terminal cmd".to_string()),
+                found: ("invalid string".to_string()),
+            })?;
+
+        let input = Console::input_paser(input);
+        if !input.chars().all(|c| c.is_alphanumeric()) {
+            self.check.read_valid = false;
+            return Err(DataError::InvalidHeader {
+                expected: ("terminal cmd".to_string()),
+                found: ("invalid string".to_string()),
+            });
         }
 
         if let ConsoleStatus::InsAcqFromTerminal = self.current_status {
-            
+            self.current_ins = Some(input.clone());
         } else {
-
+            self.current_cmd = Some(input.clone());
         }
+
+        self.interact_prompt
+            .mian_prompt
+            .push_str(&format!("{} > ", input.clone()));
+        self.check.read_valid = true;
+        
+        Ok(GenericCmd::Character(input))
     }
 
-    pub fn auto_exc_read(&mut self, prompt: &str) {
+    pub fn file_read(&mut self, prompt: &str) -> Result<GenericCmd, DataError> {
         if let ConsoleStatus::InsAcqFromFile = self.current_status {
-            
         } else {
-
         }
+        Err(DataError::Unknown)
     }
 
-    pub fn read(&mut self, prompt: &str) {
-        match self.current_status {
+    pub fn file_import(&mut self, prompt: &str) -> Result<(), DataError> {
+        self.check.file_valid = true;
+        let context = std::fs::read_to_string(prompt)?;
+
+        self.auto_exc = toml::from_str::<ExcuteFile>(&context).map_err(|_| DataError::Unknown)?;
+
+        self.check.import_valid = true;
+        println!("{:#?}", self.auto_exc);
+
+        Ok(())
+    }
+
+    pub fn read(&mut self, prompt: &str) -> Result<GenericCmd, DataError> {
+        let cmd:GenericCmd = match self.current_status {
             ConsoleStatus::InsAcqFromTerminal | ConsoleStatus::InsExecFromTerminal => {
-                self.terminal_read(prompt);
+                self.terminal_read(prompt)?
             }
             ConsoleStatus::InsAcqFromFile | ConsoleStatus::InsExecFromFile => {
-                self.auto_exc_read(prompt);
+                self.file_read(prompt)?
             }
-            ConsoleStatus::Idle => {
-                return;
+            ConsoleStatus::Invaild => { 
+                return Err(DataError::Unknown);
             }
-        }
+        };
+
+        self.refresh();
+        Ok(cmd)
+    }
+
+    /// Console state machine refresh
+    pub fn refresh(&mut self) -> Result<(), DataError> {
+        self.current_status = match self.current_status {
+            ConsoleStatus::Invaild => {
+                self.prompt_clear();
+                ConsoleStatus::InsAcqFromTerminal
+            }
+            ConsoleStatus::InsAcqFromFile => {
+                if let Some(_) = self.auto_exc.next_exc_cmd {
+                    ConsoleStatus::InsExecFromFile
+                } else if let Some(_) = self.auto_exc.next_exc_ins {
+                    self.prompt_clear();
+                    ConsoleStatus::InsAcqFromFile
+                } else {
+                    ConsoleStatus::Invaild
+                }
+            }
+            ConsoleStatus::InsAcqFromTerminal => match self.check.read_valid {
+                true => {
+                    if self.check.file_valid && self.check.import_valid {
+                        self.prompt_clear();
+                        ConsoleStatus::InsAcqFromFile
+                    } else {
+                        ConsoleStatus::InsExecFromTerminal
+                    }
+                }
+                false => {
+                        ConsoleStatus::Invaild
+                    }
+                }
+            ConsoleStatus::InsExecFromFile => {
+                if let Some(_) = self.auto_exc.next_exc_cmd {
+                    ConsoleStatus::InsExecFromFile
+                } else if let Some(_) = self.auto_exc.next_exc_ins {
+                    self.prompt_clear();
+                    ConsoleStatus::InsAcqFromFile
+                } else {
+                    ConsoleStatus::Invaild
+                }
+            }
+            ConsoleStatus::InsExecFromTerminal => {
+                if self.check.read_valid {
+                    ConsoleStatus::InsExecFromTerminal
+                } else {
+                    ConsoleStatus::Invaild
+                }
+            }
+        };
+
+        println!(
+            "
+            + - - - - - - - - - + - - - - - - - - - - - +
+            |   控制台当前状态  |  {:?}  
+            + - - - - - - - - - + - - - - - - - - - - - +
+        ",
+            self.current_status
+        );
+
+        self.check_reset();
+        Ok(())
+    }
+
+    /// Clear the console command cache
+    pub fn prompt_clear(&mut self) {
+        self.interact_prompt.mian_prompt = String::from("> ");
+        self.interact_prompt.sub_prompt = String::from("");
+    }
+
+    pub fn check_reset(&mut self) {
+        self.check.file_valid = false;
+        self.check.import_valid = false;
+        self.check.read_valid = false;
     }
 
     pub fn log(&self, log_info: &str) {
-        print!("{log_info}");
+        println!("{log_info}");
     }
 }
